1cfcf3287504e511f75b521a20eb01fe4726ac0e
tatibragel <tbrviana@hotmail.com>
2021-02-06 17:26:28 -0300

    README.md

---
+++ projetoCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculaBaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+
+            Console.WriteLine("****Calculando baskara:****");
+            Console.WriteLine("****Defina o valor de a:***");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("****Defina o valor de b:***");
+            b = double.Parse(Console.ReadLine()); 
+            Console.WriteLine("****Defina o valor de c:***");
+            c = double.Parse(Console.ReadLine()); 
+            Console.WriteLine("Os valores definidos foram");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine("delta = " + delta);
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+
+
+
+        }
+    }
+}
